e374e0880410369f14b4c847057af623c909b9a1
gskcode123 <ganeshmrr12@gmail.com>
2020-05-28 18:11:18 -0400

    update 1

--- AFexam/urls.py
+++ AFexam/urls.py
@@ -18,6 +18,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 from user import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
